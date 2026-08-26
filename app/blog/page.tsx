b8315517532f3f02a9ade0f4ba5c5aae0a4a7543
Shaurya9935 <prakhargupta9935@gmail.com>
2026-08-26 23:51:42 +0530

    Update BlogPage component to improve layout and content

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const BlogPage = () => {
   return (
-    <div>This is BlogPage</div>
+    <div>This is Blog Page
+      <div>You can write blog here.
+      </div>
+    </div>
   )
 }
 
